3d666ad50ada7be42da9b862025f4c74f3b9d572
riziq nurkholiq <101555765+RiziqAsgar@users.noreply.github.com>
2022-12-28 22:46:15 +0700

    change token

--- app/src/main/java/com/example/githubusernavigationdanapi/api/GithubApi.kt
+++ app/src/main/java/com/example/githubusernavigationdanapi/api/GithubApi.kt
@@ -11,26 +11,26 @@ import retrofit2.http.Query
 
 interface GithubApi {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: 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")
     fun getSearchUser(
         @Query("q")query: String
     ): Call<UsersResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getDetailUser(
         @Path("username") username: String
     ): Call<DetailUsers>
 
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowersUser(
         @Path("username")username: String
     ): Call<List<User>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowingUser(
         @Path("username")username: String
     ): Call<List<User>>
